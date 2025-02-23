13058b9723e3e36b6b405406a378635eaf19e158
bhaveshjadhav <bhaveshjadhav@mobiliya.io>
2025-02-24 00:14:13 +0530

    final push

--- vite-frontend/src/components/LandingPageComponent/components/Testimonials.jsx
+++ vite-frontend/src/components/LandingPageComponent/components/Testimonials.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const EmbeddableWidget = () => (
   <iframe
-    src="https://clv-wall-of-love.vercel.app/space/679f6307981adbb9346d6869/widget?bgColor=transparent&borderStyle=none&borderColor=%23000&borderWidth=1&borderRadius=0&scrollType=horizontal&cardBgColor=%23F97316&cardTextColor=%23ffffff&cardPadding=10&cardBorderRadius=20&cardShadow=true&shouldAutoScroll=true"
+    src="https://clv-wall-of-love.vercel.app/space/67bb2496588832138adde079/widget?bgColor=transparent&borderStyle=none&borderColor=%23000&borderWidth=1&borderRadius=0&scrollType=vertical&cardBgColor=%23673AB7&cardTextColor=%23ffffff&cardPadding=10&cardBorderRadius=5&cardShadow=true&shouldAutoScroll=true"
     style={{
       width: '100%',
-      height: '450px',
+      height: '400px',
       border: '1px none #000',
       borderRadius: '0px',
       backgroundColor: 'transparent',
-      overflow: 'auto',
+      overflow: 'hidden',
     }}
     title="Embeddable Widget"
   />
